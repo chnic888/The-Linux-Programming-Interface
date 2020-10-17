b5d19fd89c4a06e3f03d4518e05dcaf15c4ef1bc
chnic888 <chnic888@gmail.com>
2020-10-18 02:00:40 +0800

    add 6-3 setenv

--- chapter6/6-3/include/set_unset_env.h
+++ chapter6/6-3/include/set_unset_env.h
@@ -1,6 +1,7 @@
 #ifndef THE_LINUX_PROGRAMMING_INTERFACE_SET_UNSET_ENV_H
 #define THE_LINUX_PROGRAMMING_INTERFACE_SET_UNSET_ENV_H
-
+#include <stdio.h>
+#include <stdlib.h>
 #endif //THE_LINUX_PROGRAMMING_INTERFACE_SET_UNSET_ENV_H
 
 int setUnsetEnv(int, char *[]);
--- chapter6/6-3/src/set_unset_env.c
+++ chapter6/6-3/src/set_unset_env.c
@@ -1,32 +1,60 @@
 #include "set_unset_env.h"
-#include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <errno.h>
 
-static int setEnvironment(const char *);
+static int setEnvironment(const char *, long);
 
 static int unsetEnvironment(const char *);
 
 int setUnsetEnv(int argc, char *argv[]) {
-    if (argc != 3) {
-        fprintf(stderr, "%s set/unset k=v/k", argv[0]);
+    if (argc != 3 && argc != 4) {
+        fprintf(stderr, "%s set/unset k=v/k [overwrite]", argv[0]);
         return (EXIT_FAILURE);
     }
 
     if (strcmp(argv[1], "set") == 0) {
+        char *end = argv[3] + strlen(argv[3]);
+        long overwrite = strtol(argv[3], &end, 0);
 
-    } else {
-
+        return setEnvironment(argv[2], overwrite);
     }
+
+    if (strcmp(argv[1], "unset") == 0) {
+        return unsetEnvironment(argv[2]);
+    }
+
+    fprintf(stdout, "set/unset only");
+    return (EXIT_FAILURE);
+}
+
+static int setEnvironment(const char *envVariable, long overwrite) {
+    unsigned long size = strchr(envVariable, '=') - envVariable;
+    char *key = malloc(size);
+    strncpy(key, envVariable, size);
+
+    if (getenv(key) && overwrite == 0) {
+        fprintf(stderr, "key %s existed and overwrite is %ld\n", key, overwrite);
+        free(key);
+        key = NULL;
+        return (EXIT_SUCCESS);
+    }
+
+    char *tmpEnv = malloc(strlen(envVariable));
+    strcpy(tmpEnv, envVariable);
+    if (putenv(tmpEnv) == -1) {
+        fprintf(stderr, "failed to put env, %d\n", errno);
+        return (EXIT_FAILURE);
+    }
+
+    fprintf(stdout, "%s is set\n", getenv(key));
+
+    free(key);
+    key = NULL;
 
     return (EXIT_SUCCESS);
 }
 
-static int setEnvironment(const char *env) {
-    return 0;
-}
-
 static int unsetEnvironment(const char *envName) {
-    return 0;
+    return (EXIT_SUCCESS);
 }
 
