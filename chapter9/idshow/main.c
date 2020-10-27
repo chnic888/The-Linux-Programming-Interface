0b07d4fd8f5e109c04efd2342d7a24979014961e
chnic888 <chnic888@gmail.com>
2020-10-27 22:18:23 +0800

    add idshow

---
+++ chapter9/idshow/main.c
@@ -0,0 +1,5 @@
+#include "idshow.h"
+
+int main(int argc, char* argv[]) {
+    return idshow(argc, argv);
+}
---
+++ chapter9/idshow/src/idshow.c
@@ -0,0 +1,17 @@
+#define _GNU_SOURCE
+#include "idshow.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <grp.h>
+
+#define SG_SIZE (NG)
+
+int idshow(int argc, char* argv[]) {
+    uid_t ruid, euid, suid, fsuid;
+    gid_t rgid, egid, sgid, fsgid;
+
+
+
+    return (EXIT_SUCCESS);
+}
