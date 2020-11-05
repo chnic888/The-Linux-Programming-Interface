84026d598b70d288f04d7151dec2ad7bfb722291
chnic888 <chnic888@gmail.com>
2020-11-05 23:58:27 +0800

    update 9-1

--- chapter09/9-1/README.md
+++ chapter09/9-1/README.md
@@ -3,6 +3,10 @@
 ```c
 sudo chown root ./build/chapter09/9-1/9-1-setuids && sudo chmod u+s ./build/chapter09/9-1/9-1-setuids
 
-./build/chapter09/9-1/9-1-setuids 1000
+./build/chapter09/9-1/9-1-setuids a 2000
+./build/chapter09/9-1/9-1-setuids b 2000
+./build/chapter09/9-1/9-1-setuids c 2000
+./build/chapter09/9-1/9-1-setuids d 2000
+./build/chapter09/9-1/9-1-setuids e 2000
 ```
                                                                                                                                    
--- chapter09/9-1/src/setuids.c
+++ chapter09/9-1/src/setuids.c
@@ -5,40 +5,107 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/fsuid.h>
+#include <string.h>
 
 void showUIDs();
 
+int runA(long);
+
+int runB(long);
+
+int runC(long);
+
+int runD(long);
+
+int runE(long);
+
 int setuids(int argc, char *argv[]) {
-    if (argc != 2) {
-        fprintf(stderr, "%s uid\n", argv[0]);
+    if (argc != 3) {
+        fprintf(stderr, "%s a/b/c/d/e uid\n", argv[0]);
         return (EXIT_FAILURE);
     }
     showUIDs();
 
     char *endptr = NULL;
-    long uid = strtol(argv[1], &endptr, 10);
+    long uid = strtol(argv[2], &endptr, 10);
+    int ret;
 
-    fprintf(stdout, "== setuid(%d) ==\n", (int) uid);
-    if (setuid(uid) == -1) {
-        fprintf(stderr, "failed to setuid\n");
+    switch (argv[1][0]) {
+        case 'a':
+            ret = runA(uid);
+            break;
+        case 'b':
+            ret = runB(uid);
+            break;
+        case 'c':
+            ret = runC(uid);
+            break;
+        case 'd':
+            ret = runD(uid);
+            break;
+        case 'e':
+            ret = runE(uid);
+            break;
+        default:
+            fprintf(stderr, "invalid command\n");
+            ret = -1;
+    }
+
+    return ret == -1 ? EXIT_FAILURE : EXIT_SUCCESS;
+}
+
+int runE(long uid) {
+    fprintf(stdout, "==setresuid(-1, %d, %d)==\n", (int) uid, (int) (uid + 1000));
+    if (setresuid(-1, uid, uid + 1000) == -1) {
+        fprintf(stderr, "failed to setresuid\n");
         return (EXIT_FAILURE);
     }
-    showUIDs();
 
-    fprintf(stdout, "==setreuid(–1, %d)==\n", (int) uid);
-    if (setreuid(-1, uid) == -1) {
-        fprintf(stderr, "failed to setreuid\n");
+    showUIDs();
+    return (EXIT_SUCCESS);
+}
+
+int runD(long uid) {
+    fprintf(stdout, "==setfsuid(%d)==\n", (int) uid);
+    if (setfsuid(uid) == -1) {
+        fprintf(stderr, "failed to setfsuid\n");
         return (EXIT_FAILURE);
     }
-    showUIDs();
 
+    showUIDs();
+    return (EXIT_SUCCESS);
+}
+
+int runC(long uid) {
     fprintf(stdout, "==seteuid(%d)==\n", (int) uid);
     if (seteuid(uid) == -1) {
         fprintf(stderr, "failed to seteuid\n");
         return (EXIT_FAILURE);
     }
-    showUIDs();
 
+    showUIDs();
+    return (EXIT_SUCCESS);
+}
+
+int runB(long uid) {
+    fprintf(stdout, "==setreuid(–1, %d)==\n", (int) uid);
+    if (setreuid(-1, uid) == -1) {
+        fprintf(stderr, "failed to setreuid\n");
+        return (EXIT_FAILURE);
+    }
+
+    showUIDs();
+    return (EXIT_SUCCESS);
+}
+
+int runA(long uid) {
+    fprintf(stdout, "== setuid(%d) ==\n", (int) uid);
+    if (setuid(uid) == -1) {
+        fprintf(stderr, "failed to setuid\n");
+        return (EXIT_FAILURE);
+    }
+
+    showUIDs();
     return (EXIT_SUCCESS);
 }
 
