0aa041a2e1ac221dbbc02171b750383430e29034
chnic888 <chnic888@gmail.com>
2020-11-07 00:57:18 +0800

    finish calendar_time

--- chapter10/calendar-time/src/calendar_time.c
+++ chapter10/calendar-time/src/calendar_time.c
@@ -20,7 +20,7 @@ int calendarTime(int argc, char *argv[]) {
         fprintf(stderr, "failed to call gettimeofday(), %d\n", errno);
         return (EXIT_FAILURE);
     }
-    fprintf(stdout, " gettimeofday() returned %ld secs, %ld microsecs\n", (long) tv.tv_sec, (long) tv.tv_usec);
+    fprintf(stdout, "   gettimeofday() returned %ld secs, %ld microsecs\n", (long) tv.tv_sec, (long) tv.tv_usec);
 
     gmp = gmtime(&t);
     if (gmp == NULL) {
@@ -29,10 +29,26 @@ int calendarTime(int argc, char *argv[]) {
     }
     gm = *gmp;
     fprintf(stdout, "Broken down by gmtime():\n");
-    fprintf(stdout, " year=%d mon=%d mday=%d hour=%d min=%d sec=%d ", gm.tm_year, gm.tm_mon, gm.tm_mday, gm.tm_hour,
+    fprintf(stdout, "   year=%d mon=%d mday=%d hour=%d min=%d sec=%d ", gm.tm_year, gm.tm_mon, gm.tm_mday, gm.tm_hour,
             gm.tm_min, gm.tm_sec);
     fprintf(stdout, "wday=%d yday=%d isdst=%d\n", gm.tm_wday, gm.tm_yday, gm.tm_isdst);
 
+    locp = localtime(&t);
+    if (locp == NULL) {
+        fprintf(stderr, "failed to call localtime(), %d\n", errno);
+        return (EXIT_FAILURE);
+    }
+
+    loc = *locp;
+    fprintf(stdout, "Broken down by localtime():\n");
+    fprintf(stdout, "   year=%d mon=%d mday=%d hour=%d min=%d sec=%d ", loc.tm_year, loc.tm_mon, loc.tm_mday,
+            loc.tm_hour, loc.tm_min, loc.tm_isdst);
+
+    fprintf(stdout, "asctime() formats the gmtime() value as : %s", asctime(&gm));
+    fprintf(stdout, "ctime() formats the time() values as:     %s", ctime(&t));
+
+    fprintf(stdout, "mktime() of gmtime() value:    %ld secs\n", (long) mktime(&gm));
+    fprintf(stdout, "mktime() of localtime() value: %ld secs\n", (long) mktime(&loc));
 
     return (EXIT_SUCCESS);
 }
