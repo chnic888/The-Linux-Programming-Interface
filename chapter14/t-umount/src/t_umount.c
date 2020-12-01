4315a909cfdd6ffa142bf22132d8deef1aed4e9c
chnic888 <chnic888@gmail.com>
2020-12-01 23:23:48 +0800

    finished chapter 14 t-umount

--- chapter14/t-umount/src/t_umount.c
+++ chapter14/t-umount/src/t_umount.c
@@ -1,7 +1,20 @@
 #include "t_umount.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/mount.h>
+#include <errno.h>
 
 int tUmount(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "%s target\n", argv[0]);
+        return (EXIT_FAILURE);
+    }
+
+    if (umount(argv[1]) == -1) {
+        fprintf(stderr, "failed to call umount(), %d\n", errno);
+        return (EXIT_FAILURE);
+    }
+
+    fprintf(stdout, "umount %s successfully\n", argv[1]);
     return (EXIT_SUCCESS);
 }
