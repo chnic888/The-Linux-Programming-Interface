f94d6e19377f50291a5f172a7b7434878a77e23e
chnic888 <chnic888@gmail.com>
2020-12-07 14:53:22 +0800

    add chapter 14 t_statvfs

--- chapter14/t-statvfs/src/t_statvfs.c
+++ chapter14/t-statvfs/src/t_statvfs.c
@@ -2,8 +2,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
-#include <sys/mount.h>
+#include <sys/statvfs.h>
 
 int tStatvfs(int argc, char *argv[]) {
-    return 0;
+    if (argc != 2) {
+        fprintf(stderr, "%s pathname\n", argv[0]);
+        return (EXIT_FAILURE);
+    }
+
+    struct statvfs sv;
+    if (statvfs(argv[1], &sv) == -1) {
+        fprintf(stderr, "failed to call statvfs(), %d\n", errno);
+        return (EXIT_FAILURE);
+    }
+
+    fprintf(stdout, "File-system block size                                     %ld\n", sv.f_bsize);
+    fprintf(stdout, "Fundamental file-system block size                         %ld\n", sv.f_frsize);
+    fprintf(stdout, "Total number of blocks in file system                      %d\n", sv.f_blocks);
+    fprintf(stdout, "Total number of free blocks                                %d\n", sv.f_bfree);
+    fprintf(stdout, "Number of free blocks available to unprivileged process    %d\n", sv.f_bavail);
+    fprintf(stdout, "Total number of i-nodes                                    %d\n", sv.f_files);
+    fprintf(stdout, "Total number of free i-nodes                               %d\n", sv.f_ffree);
+    fprintf(stdout, "Number of i-nodes available to unprivileged process        %d\n", sv.f_favail);
+    fprintf(stdout, "File-system ID                                             %ld\n", sv.f_fsid);
+    fprintf(stdout, "Mount flags                                                %ld\n", sv.f_flag);
+    fprintf(stdout, "Maximum length of filenames on this file system            %ld\n", sv.f_namemax);
+
+    return (EXIT_SUCCESS);
 }
