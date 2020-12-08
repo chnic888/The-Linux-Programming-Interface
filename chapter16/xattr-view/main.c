fbbf40ed9d63ced90bbcaf1b9fdea0e6844d631f
chnic888 <chnic888@gmail.com>
2020-12-09 01:26:29 +0800

    add chapter 16 xattr-view

---
+++ chapter16/xattr-view/main.c
@@ -0,0 +1,5 @@
+#include "xattr_view.h"
+
+int main(int argc, char *argv[]) {
+    return xattrView(argc, argv);
+}
---
+++ chapter16/xattr-view/src/xattr_view.c
@@ -0,0 +1,12 @@
+#include "xattr_view.h"
+#include <sys/xattr.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <stdbool.h>
+
+#define XATTR_SIZE 10000
+
+int xattrView(int argc, char *argv[]) {
+    return (EXIT_SUCCESS);
+}
