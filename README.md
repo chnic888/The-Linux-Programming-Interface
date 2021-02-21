4dc229b65ee62edc0b5a983ad756729ad5739d10
chnic888 <chnic888@gmail.com>
2021-02-22 03:46:10 +0800

    refactoring build script

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # The-Linux-Programming-Interface
 The-Linux-Programming-Interface: A Linux and Unix System Programming Handbook
 
-
 ## Build Executable File
 ```shell script
+# build all
 sh ./build.sh
+
+#build by range
+sh ./build.sh start end
 ```
--- build.sh
+++ build.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 
 BUILD_DIR='./build'
+START=3
+END=24
+
+if [ $# -eq 1 ]; then
+    START="$1"
+fi
+
+if [ $# -eq 2 ]; then
+    END="$2"
+fi
 
 if [ -d "$BUILD_DIR" ]; then
   printf "build folder cleanup\n"
@@ -9,5 +19,5 @@ fi
 
 mkdir "$BUILD_DIR"
 cd "$BUILD_DIR" || exit
-cmake ..
+cmake .. -DSTART="$START" -DEND="$END"
 make
