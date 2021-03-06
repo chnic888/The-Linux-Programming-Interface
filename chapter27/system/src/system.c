dd68943c426a27a207f71ace42a7cf36d60dcaf3
chnic888 <chnic888@gmail.com>
2021-03-06 23:37:55 +0800

    finish chapter27 system

--- chapter27/system/src/system.c
+++ chapter27/system/src/system.c
@@ -6,5 +6,65 @@
 #include <errno.h>
 
 int system(const char *command) {
-    return 0;
+    sigset_t blockMask, origMask;
+    struct sigaction saIgnore, saOrigQuit, saOrigInt, saDefault;
+    pid_t childPid;
+    int status, savedErrno;
+
+    if (command == NULL) {
+        return system(":") == 0;
+    }
+
+    sigemptyset(&blockMask);
+    //block SIGCHILD
+    sigaddset(&blockMask, SIGCHLD);
+    sigprocmask(SIG_BLOCK, &blockMask, &origMask);
+
+    //ignore SIG_INT and SIG_QUIT
+    saIgnore.sa_handler = SIG_IGN;
+    saIgnore.sa_flags = 0;
+    sigemptyset(&saIgnore.sa_mask);
+    sigaction(SIGINT, &saIgnore, &saOrigInt);
+    sigaction(SIGQUIT, &saIgnore, &saOrigQuit);
+
+    switch (childPid = fork()) {
+        case -1:
+            status = -1;
+            break;
+        case 0:
+            saDefault.sa_handler = SIG_DFL;
+            saDefault.sa_flags = 0;
+            sigemptyset(&saDefault.sa_mask);
+
+            if (saOrigInt.sa_handler != SIG_IGN) {
+                sigaction(SIGINT, &saDefault, NULL);
+            }
+
+            if (saOrigQuit.sa_handler != SIG_IGN) {
+                sigaction(SIGQUIT, &saDefault, NULL);
+            }
+
+            sigprocmask(SIG_SETMASK, &origMask, NULL);
+
+            execl("/bin/sh", "sh", "-c", command, (char *) NULL);
+            _exit(127);
+        default:
+            while (waitpid(childPid, &status, 0) == -1) {
+                if (errno != EINTR) {
+                    status = -1;
+                    break;
+                }
+            }
+            break;
+    }
+
+    savedErrno = errno;
+
+    sigprocmask(SIG_SETMASK, &origMask, NULL);
+    sigaction(SIGINT, &saOrigInt, NULL);
+    sigaction(SIGQUIT, &saOrigQuit, NULL);
+
+    errno = savedErrno;
+
+    return status;
 }
