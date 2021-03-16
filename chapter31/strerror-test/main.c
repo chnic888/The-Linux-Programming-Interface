ebddbb7dcb9cbacbfe21378faeee869f873f271f
chnic888 <chnic888@gmail.com>
2021-03-16 23:50:10 +0800

    add strerror-test

--- chapter31/strerror-test/main.c
+++ chapter31/strerror-test/main.c
@@ -1,6 +1,5 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "strerror_test.h"
 
 int main(int argc, char *argv[]) {
-    return 0;
+    return strerrorTest(argc, argv);
 }
--- chapter31/strerror-test/src/strerror_test.c
+++ chapter31/strerror-test/src/strerror_test.c
@@ -1,9 +1,38 @@
 #include "strerror_test.h"
+
 #include <stdio.h>
 #include <string.h>
 #include <pthread.h>
 #include <errno.h>
-#include <unistd.h>
+#include <stdlib.h>
 
+static void *threadFunc(void *arg) {
+    char *str;
+    fprintf(stdout, "Other thread about to call strerror()\n");
+    str = strerror(EPERM);
+    fprintf(stdout, "Other thread: str (%p) = %s\n", str, str);
+}
 
+int strerrorTest(int argc, char *argv[]) {
+    pthread_t t;
+    int s;
+    char *str;
 
+    str = strerror(EINVAL);
+    fprintf(stdout, "Main thread has called strerror()\n");
+
+    s = pthread_create(&t, NULL, threadFunc, NULL);
+    if (s != 0) {
+        fprintf(stderr, "failed to call pthread_create(), %d\n", errno);
+        exit(EXIT_FAILURE);
+    }
+
+    s = pthread_join(t, NULL);
+    if (s != 0) {
+        fprintf(stderr, "failed to call pthread_join(), %d\n", errno);
+        exit(EXIT_FAILURE);
+    }
+
+    fprintf(stdout, "Main thread: str (%p) = %s\n", str, str);
+    exit(EXIT_SUCCESS);
+}
