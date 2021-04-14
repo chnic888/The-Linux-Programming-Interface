5597cd1ebdd2a82ea2925c5d360458289a0fc94f
chnic888 <chnic888@gmail.com>
2021-04-14 23:18:00 +0800

    add rlimit-nproc

--- chapter36/rlimit-nproc/src/print_rlimit.c
+++ chapter36/rlimit-nproc/src/print_rlimit.c
@@ -1,5 +1,33 @@
 #include "print_rlimit.h"
+#include <sys/resource.h>
+#include <stdio.h>
 
 int printRlimit(const char *msg, int resource) {
+    struct rlimit rlim;
+
+    if (getrlimit(resource, &rlim) == -1) {
+        return -1;
+    }
+
+    fprintf(stdout, "%s soft=", msg);
+    if (rlim.rlim_cur == RLIM_INFINITY)
+        fprintf(stdout, "infinite");
+#ifdef RLIM_SAVED_CUR
+    else if (rlim.rlim_cur == RLIM_SAVED_CUR)
+        fprintf(stdout, "unrepresentable");
+#endif
+    else
+        fprintf(stdout, "%lld", (long long) rlim.rlim_cur);
+
+    fprintf(stdout, "; hard=");
+    if (rlim.rlim_max == RLIM_INFINITY)
+        fprintf(stdout, "infinite\n");
+#ifdef RLIM_SAVED_MAX
+    else if (rlim.rlim_max == RLIM_SAVED_MAX)
+        fprintf(stdout, "unrepresentable");
+#endif
+    else
+        fprintf(stdout, "%lld\n", (long long) rlim.rlim_max);
+
     return 0;
 }
--- chapter36/rlimit-nproc/src/rlimit_nproc.c
+++ chapter36/rlimit-nproc/src/rlimit_nproc.c
@@ -1,5 +1,48 @@
 #include "rlimit_nproc.h"
+#include "print_rlimit.h"
+#include <sys/resource.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <errno.h>
 
 int rlimitNproc(int argc, char *argv[]) {
-    return 0;
+    struct rlimit rl;
+    int j;
+    pid_t childPid;
+    char *endPtr = NULL;
+
+    if (argc < 2 || argc > 3 || strcmp(argv[1], "--help") == 0) {
+        fprintf(stderr, "%s soft-limit [hard-limit]\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    printRlimit("Initial maximum process limits: ", RLIMIT_NPROC);
+
+    rl.rlim_cur = (argv[1][0] == 'i') ? RLIM_INFINITY : strtol(argv[1], &endPtr, 10);
+    endPtr = NULL;
+
+    rl.rlim_max = (argc == 2) ? rl.rlim_cur : (argv[1][0] == 'i') ? RLIM_INFINITY : strtol(argv[2], &endPtr, 10);
+    endPtr = NULL;
+
+    if (setrlimit(RLIMIT_NPROC, &rl) == -1) {
+        fprintf(stderr, "failed to call setrlimit(), %d\n", errno);
+        exit(EXIT_FAILURE);
+    }
+
+    printRlimit("New maximum process limits:     ", RLIMIT_NPROC);
+
+    for (j = 1; j < 10; j++) {
+        switch (childPid = fork()) {
+            case -1:
+                fprintf(stderr, "failed to call fork(), %d\n", errno);
+                exit(EXIT_FAILURE);
+            case 0:
+                _exit(EXIT_SUCCESS);
+            default:
+                fprintf(stdout, "Child %d (PID=%ld) started\n", j, (long) childPid);
+                break;
+        }
+    }
 }
