d4754457086e9761964c8e4d971471cedf740197
chnic888 <chnic888@gmail.com>
2021-06-27 00:41:41 +0800

    add simgple_pipe

--- chapter44/simple-pipe/src/simple_pipe.c
+++ chapter44/simple-pipe/src/simple_pipe.c
@@ -27,9 +27,55 @@ int simplePipe(int argc, char *argv[]) {
         case -1:
             fprintf(stderr, "failed to call folk(), %d\n", errno);
             exit(EXIT_FAILURE);
+
         case 0:
+            if (close(pfd[1]) == -1) {
+                fprintf(stderr, "[child]failed to clod the write fd, %d\n", errno);
+                exit(EXIT_FAILURE);
+            }
+
+            for (;;) {
+                numRead = read(pfd[0], buf, BUf_SIZE);
+                if (numRead == -1) {
+                    fprintf(stderr, "[child]failed to clod the write fd, %d\n", errno);
+                    exit(EXIT_FAILURE);
+                }
+
+                if (numRead == 0) {
+                    break;
+                }
+
+                if (write(STDOUT_FILENO, buf, numRead) != numRead) {
+                    fprintf(stderr, "[child]failed to write string to stdout, %d\n", errno);
+                    exit(EXIT_FAILURE);
+                }
+            }
+
+            write(STDOUT_FILENO, "\n", 1);
+            if (close(pfd[0]) == -1) {
+                fprintf(stderr, "[child]failed to clod the read fd, %d\n", errno);
+                exit(EXIT_FAILURE);
+            }
+
             _exit(EXIT_SUCCESS);
+
         default:
+            if (close(pfd[0]) == -1) {
+                fprintf(stderr, "[parent]failed to clod the read fd, %d\n", errno);
+                exit(EXIT_FAILURE);
+            }
+
+            if (write(pfd[1], argv[1], strlen(argv[1])) != strlen(argv[1])) {
+                fprintf(stderr, "[parent]failed to write the pipe, %d\n", errno);
+                exit(EXIT_FAILURE);
+            }
+
+            if (close(pfd[1]) == -1) {
+                fprintf(stderr, "[parent]failed to clod the write fd, %d\n", errno);
+                exit(EXIT_FAILURE);
+            }
+
+            wait(NULL);
             exit(EXIT_SUCCESS);
     }
 }
